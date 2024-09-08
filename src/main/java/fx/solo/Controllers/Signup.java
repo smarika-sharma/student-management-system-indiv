7446afc1852c2b06c98e269ae40a0162c6468e2e
Smarika Sharma <smarikasharma89@gmail.com>
2024-09-08 08:18:09 +0545

    Validated full name

--- src/main/java/fx/solo/Controllers/Signup.java
+++ src/main/java/fx/solo/Controllers/Signup.java
@@ -14,70 +14,93 @@ import static fx.solo.Uses.Uses.changeScene;
 import static fx.solo.Uses.Uses.error;
 
 public class Signup {
-@FXML
-private TextField signupFullName;
-@FXML
-private ChoiceBox<String> signupFaculty;
-@FXML
-private ChoiceBox<String> signupGender;
-@FXML
-private TextField signupEmail;
-@FXML
-private TextField signupPhoneNumber;
-@FXML
-private PasswordField signupPassword;
-@FXML
-private PasswordField signupConformPassword;
-@FXML
-private Label signupErrorLabel;
+    @FXML
+    private TextField signupFullName;
+    @FXML
+    private ChoiceBox<String> signupFaculty;
+    @FXML
+    private ChoiceBox<String> signupGender;
+    @FXML
+    private TextField signupEmail;
+    @FXML
+    private TextField signupPhoneNumber;
+    @FXML
+    private PasswordField signupPassword;
+    @FXML
+    private PasswordField signupConformPassword;
+    @FXML
+    private Label signupErrorLabel;
 
     @FXML
-    protected void initialize(){
-        final String[] faculty= {"BCS", "BHM", "BBA", "BIHM", "MCS", "MBA"};
+    protected void initialize() {
+        final String[] faculty = {"BCS", "BHM", "BBA", "BIHM", "MCS", "MBA"};
         signupFaculty.getItems().addAll(faculty);  //adding the dropdown
-        final String[] gender={"Male", "Female", "Others"};
+        final String[] gender = {"Male", "Female", "Others"};
         signupGender.getItems().addAll(gender);
         signupErrorLabel.setText("");
         addInputListeners();
     }
 
-    protected void addInputListeners(){
+    protected void addInputListeners() {
         signupFullName.textProperty().addListener((_) -> clearErrorLabels());
         signupEmail.textProperty().addListener((_) -> clearErrorLabels());
         signupPhoneNumber.textProperty().addListener((_) -> clearErrorLabels());
     }
 
-    protected void clearErrorLabels(){
+    protected void clearErrorLabels() {
         signupErrorLabel.setText("");
     }
 
     @FXML
     protected void signupBtnClicked() {
-    String fullName= signupFullName.getText();
-    String email= signupEmail.getText();
-    String faculty= signupFaculty.getValue();
-    String gender= signupGender.getValue();
-    String phoneNumber= signupPhoneNumber.getText();
-    String password= signupPassword.getText();
-    String cPassword= signupConformPassword.getText();
+        String fullName = signupFullName.getText();
+        String email = signupEmail.getText();
+        String faculty = signupFaculty.getValue();
+        String gender = signupGender.getValue();
+        String phoneNumber = signupPhoneNumber.getText();
+        String password = signupPassword.getText();
+        String cPassword = signupConformPassword.getText();
+        
+        System.out.println(faculty); //if faculty field is empty, it prints 'null' to the console
+        System.out.println(email);  //even if i put email blank, it prints nothing.
 
-    if(fullName.isEmpty()|| email.isEmpty() || faculty.isEmpty()||gender.isEmpty()||phoneNumber.isEmpty()||password.isEmpty()||cPassword.isEmpty()){
-        error(signupErrorLabel, "Field/s is empty", true);
+//        here isEmpty() did not work for choiceboxes
+        if (fullName.isEmpty() || email.isEmpty() || faculty == null || gender == null || phoneNumber.isEmpty() || password.isEmpty() || cPassword.isEmpty()) {
+            error(signupErrorLabel, "Field/s is empty", true);
+        }
+//        when all fields are filled then this block is executed to verify all fields.
+        else {
+            validateFullName();
+            validateEmail();
+        }
     }
-//    int validateLength= fullName.length();
-//    if(validateLength<5){
-//        error(signupErrorLabel, "Full Name length not acceptable !", true);
-//    }
-//    else {
-//        if (fullName.charAt(0) == ' '|| fullName.charAt(-1) == ' ') {
-//            error(signupErrorLabel, "Invalid name", true);
-//        }
-//        else {
-////            there is at least a space in the full name.
-//            for (int i = 0; i <= validateLength; i++) {
-//            }
-//        }
-//    }
+
+    private void validateFullName() {
+        String fullName = signupFullName.getText();
+
+//            a. length must be greater than 5
+        int validateLength = fullName.length();
+        if (validateLength < 5) {
+            error(signupErrorLabel, "Full Name length not acceptable !", true);
+        }
+//            b. There cannot be space at the 0th or last index in name
+        else if (fullName.charAt(0) == ' ' || fullName.charAt(validateLength - 1) == ' ') {
+            error(signupErrorLabel, "Invalid full name", true);
+        }
+//            c. There should be at least a space in the full name
+        int numOfSpacesInName = 0;
+        for (int i = 1; i <= validateLength - 2; i++) {
+            if (fullName.charAt(i) == ' ') {
+                numOfSpacesInName++;
+            }
+        }
+        if (numOfSpacesInName == 0) {
+            error(signupErrorLabel, "Provide a valid full name", true);
+        }
+    }
+
+    private void validateEmail(){
+
     }
 
     @FXML
